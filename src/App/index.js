fbf5436ace387264b386c81f30c28e3a6e27140e
heinburger <steven.c.hein@gmail.com>
2018-06-25 16:14:24 +0200

    Fix from defaults

---
+++ src/App/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        whatup
+      </div>
+    );
+  }
+}
+
+export default App;
