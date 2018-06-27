b13034dc49e9278460c0650281a3994ef10e8924
heinburger <steven.c.hein@gmail.com>
2018-06-27 15:12:35 +0200

    Fix tests

--- src/Layout/Bar.test.js
+++ src/Layout/Bar.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import Bar from './Bar';
 import LayoutStore from './store';
 
 it('renders without crashing', () => {
-  mount(
+  shallow(
     <Bar layout={ new LayoutStore() }>
       <div>smack</div>
     </Bar>
