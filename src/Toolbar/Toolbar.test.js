2c1d562ef3f6dc4c60942bc7b38dae158d67dd36
heinburger <steven.c.hein@gmail.com>
2018-07-01 13:10:15 +0200

    More clear test store injection

--- src/Toolbar/Toolbar.test.js
+++ src/Toolbar/Toolbar.test.js
@@ -2,12 +2,9 @@ import React from 'react';
 import { mount } from 'enzyme';
 import Toolbar from './Toolbar';
 import StoreProvider from '../App/StoreProvider';
-import AppStore from '../App/store';
+import LayoutStore from '../Layout/store';
+import ThemeStore from '../Theme/store';
 
 it('renders without crashing', () => {
-  mount(
-    <StoreProvider store={ new AppStore() }>
-      <Toolbar />
-    </StoreProvider>
-  );
+  mount(<Toolbar layout={ new LayoutStore() } theme={ new ThemeStore() } />);
 });
